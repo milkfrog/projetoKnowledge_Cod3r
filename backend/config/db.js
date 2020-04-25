23fbd86ad8c933de3e182d250e1926e3cf3219f9
milkfrog <contin.marcelo@gmail.com>
2020-04-25 19:00:24 -0300

    Add aula 508

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 const config = require('../knexfile.js')
 const knex = require('knex')(config)
 
+// executa as migrations no momento que inicia o backend
+knex.migrate.latest([config])
+console.log('Rodando as migrations no Banco de Dados')
+
 module.exports = knex
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,9 +1,20 @@
+// postgres no ubuntu roda na porta 5433, e nao na 5432!!
+// usar comando sudo lsof -i -P -n | grep LISTEN
+// para ver quais portas estao sendo usadas
+
+// error: password authentication failed for user "postgres"
+// eu resolvi criando um super usuario no postgresql da seguinte forma:
+// 1) CREATE USER nomedousuario SUPERUSER INHERIT CREATEDB CREATEROLE;
+// 2) ALTER USER nomedousuario PASSWORD 'novaSenha';
+
+
 module.exports = {
 	client: 'postgresql',
 	connection: {
 		database: 'knowledge',
-		user:     'postgres',
-		password: '123456'
+		port: 5433,
+		user:     'admin',
+		password: 'admin'
 	},
 	pool: {
 		min: 2,
