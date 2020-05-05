5b021e92c394c67e71d7aa3f4f7b975dc1de5a19
milkfrog <contin.marcelo@gmail.com>
2020-05-05 09:51:50 -0300

    Add aula 524

---
+++ frontend/src/components/template/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <aside class="menu">
+
+    </aside>
+</template>
+
+<script>
+export default {
+    name: 'menu'
+}
+</script>
+
+<style>
+    .menu {
+        grid-area: menu;
+        background: linear-gradient(to right, #232526, #414345);
+
+        display: flex;
+        flex-direction: column;
+        /* flex-wrap permite quebra de linha */
+        flex-wrap: wrap;
+    }
+</style>
