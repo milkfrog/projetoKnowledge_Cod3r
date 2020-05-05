1ad15e568d2ac0e3f4f9c18ced83f6c5edcb678c
milkfrog <contin.marcelo@gmail.com>
2020-05-05 10:47:42 -0300

    Add aula 526

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -18,12 +18,13 @@ export default {
     },
     computed: {
         icon() {
-            return "fa-angle-left"
+            return this.$store.state.isMenuVisible ? "fa-angle-left" : "fa-angle-down"
         }
     },
     methods: {
         toggleMenu() {
-            
+            // chama pelo Vuex a mutation
+            this.$store.commit('toggleMenu')
         }
     }
 }
