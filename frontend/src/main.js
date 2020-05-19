c17c6799731a346708b7fdeb20e036d216af464b
milkfrog <contin.marcelo@gmail.com>
2020-05-19 18:01:35 -0300

    Add aula 537

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // TEMPORARIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6ImNvbnRpbi5tYXJjZWxvQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODk1NDU4NDUsImV4cCI6MTU4OTgwNTA0NX0.T5QlCVLlFwwuDYfEmMvz6X8CIFEotCvLZyQ1LCO84ws'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6ImNvbnRpbi5tYXJjZWxvQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODk5MjE3NTIsImV4cCI6MTU5MDE4MDk1Mn0.12fYMeJsHWBgE-C2RB5Vfp3JH_2J_rEHmKwUJyXG_-E'
 
 new Vue({
   store,
